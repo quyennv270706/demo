b3e0c8ec5c1578e62c07ce8e31572f62df2c7a81
quyennv <quyennv19122006@gmail.com>
2024-09-26 17:54:16 +0700

    init

--- main.cpp
+++ main.cpp
@@ -1,34 +1,19 @@
-// C++ program to convert a decimal
-// number to binary number
-
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
-// function to convert decimal to binary
-void decToBinary(int n)
-{
-    // array to store binary number
-    int binaryNum[32];
-
-    // counter for binary array
-    int i = 0;
-    while (n > 0) {
-
-        // storing remainder in binary array
-        binaryNum[i] = n % 2;
-        n = n / 2;
-        i++;
-    }
-
-    // printing binary array in reverse order
-    for (int j = i - 1; j >= 0; j--)
-        cout << binaryNum[j];
-}
-
-// Driver program to test above function
 int main()
 {
-    int n = 17;
-    decToBinary(n);
+    ll x;
+    cin >> x;
+    int idx = 0;
+    int n = 0;
+    while (x)
+    {
+        n += (x % 10) * pow(2, idx);
+        idx++;
+        x /= 10;
+    }
+    cout << n;
     return 0;
 }
